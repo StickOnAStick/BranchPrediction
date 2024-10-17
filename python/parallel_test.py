44a8a2134e13d26e4ac02db6d78e5e209449c060
Nicholas T Dimidik <nicholas.dimidik@sjsu.edu>
2024-10-16 19:32:08 -0700

    Cleaned a bit.

--- python/parallel_test.py
+++ python/parallel_test.py
@@ -1,3 +1,9 @@
+
+from test_parser import create_csv, display_graph
+from recompiler import compile_all
+from loguru import logger
+from argparse import Namespace
+
 import subprocess
 import os
 import time
@@ -5,15 +11,29 @@ import sys
 import threading
 import psutil
 import json
-from test_parser import create_csv, display_graph
-from recompiler import compile_all
+import pathlib
+import argparse
 
-# What I want for this file: Run a command line with different predictors, if that predictor does not exist, generate the executable, if it does already exist do not generate the executable
-# Need some way to figure out if the executable is up to date, I could just try using the make, becuase once the json is created we can just do the make 
+"""
+What I want for this file: 
+ Run a command line with different predictors, if that predictor does not exist, 
+ generate the executable, if it does already exist do not generate the executable
+ 
+ TODO:  Some way to figure out if the executable is up to date, I could just try 
+        using the make, becuase once the json is created we can just do the make 
+"""
 
+MAIN_PATH           = pathlib.Path(__file__).parent.parent.resolve()
+LOG_PATH            = MAIN_PATH.joinpath("python/logs")
+CHAMPSIM_EXE_PATH   = MAIN_PATH.joinpath("ChampSim/bin/champsim")
+TRACER_PATH         = MAIN_PATH.joinpath("ChampSim/tracer")
+PREDICTOR_PATH      = MAIN_PATH.joinpath("ChampSim/branch")
 
-os.chdir("..")
-print(os.getcwd())
+assert LOG_PATH.is_dir()
+assert CHAMPSIM_EXE_PATH.is_file()
+assert TRACER_PATH.is_dir()
+assert PREDICTOR_PATH.is_dir()
+
 def memchecker(pids,log):
     time.sleep(10)
     for p in pids:
@@ -35,75 +55,105 @@ predictor = 'branch'      # path for predictors
 
 run_predictors = []
 recompile_list = []
-predictor_list = os.listdir(predictor)
+predictor_list: list[str] = os.listdir(PREDICTOR_PATH)
 # print(predictor_list)
 # Take command line arguments 
 args = len(sys.argv)
 
-count = 0
 recompile = False
-print("use --help to see avaliable commands")
-for i in sys.argv:
-    count += 1
-    if i == "--warmup-instructions":
-        warmup_instructions = int(sys.argv[count])
-    elif i == "--simulation_instructions":
-        simulated_instructions = int(sys.argv[count])
-    elif i == "--all":
-        recompile = True
+print("\n\n------------ChampSim Parallel Test Launcher----------")
+
+def main():
+
+    args: Namespace = parse_args()
+
+    recompile = config_setup(args=args, recompile=recompile)
+
+    if recompile:
+        logger.info(f"Recompiling the following predictors: {recompile_list}")
+        compile_all(recompile_list)
+    logger.info(f"Running the following predictors: {run_predictors}")
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Claros CLI parser")
+
+     # Define arguments with their respective options
+    parser.add_argument('--warmup_instructions', type=int, help='Number of warmup instructions to run')
+    parser.add_argument('--simulation_instructions', type=int, help='Number of total instructions to run')
+    parser.add_argument('--all', action='store_true', help='Compile and run all available predictors')
+    parser.add_argument('--predictors', nargs='+', help='List of predictors to run')
+    parser.add_argument('--recompile', nargs='+', help='List of predictors to recompile')
+    parser.add_argument('--help', help="View list of instructions") # Kinda pointless now? Provides better formatting
+
+    return parser.parse_args()
+
+def config_setup(args: Namespace, recomp: bool = False) -> bool:
+    """Function to config your champsim run"""
+    global warmup_instructions, simulated_instructions
+
+    args = parse_args()
+        
+    if args.warmup_instructions:
+        try:
+            int(args.warmup_instructions)
+        except ValueError:
+            logger.error("Wrong type of value passed into warmup_instructions!")
+        warmup_instructions = int(args.warmup_instructions)
+
+    if args.simulation_instructions:
+        try:
+            int(args.simulation_instructions)
+        except ValueError:
+            logger.error("Wrong type of value passed into --simulation_instructions")
+        simulated_instructions = int(args.simulation_instructions)
+
+    if args.predictors:
+        if "all" in args.predictors:
+            run_predictors = predictor_list
+        else:
+            for predictor in args.predictors:
+                if predictor in predictor_list:
+                    run_predictors.append(predictor)
+                else:
+                    logger.error(f"Predictor {predictor} not found in predictor list!")
+                    SystemError(f"Predictor not found {predictor}")
+
+    if args.recompile:
+        if "all" in args.recompile:
+            recompile_list = predictor_list
+        else:
+            for predictor in args.predictors:
+                if predictor in recompile_list:
+                    recompile_list.append(predictor)
+                else:
+                    logger.error(f"Recompile {predictor} not found in recompile list!")
+                    SystemError(f"Predictor not found for recompilation {predictor}")
+
+        recomp = True
+
+    if args.help:
+        print ("Avaliable instructions \n" +
+                "--warmup-instructions : Number of warmup instructions to run \n" +
+                "--simulation_instructions : Number of total instructions to run \n" +
+                "--all: compiles and runs all avaliable predictors\n" + 
+                "--predictors: list the predictors you want to run in the format --predictors predictor1 predictor2 ... \n" +
+                "--predictors all: Runs all avaliable predictors\n" +
+                "--recompile: Recompiles the instances of champsim, enable this if you have changed any of the predictor files. this in the format --recompile predictor1 predictor2 \n" +
+                "--recompile all: Recompiles all avaliable predictors, use this command the first time you run the program")
+
+    if args.all: # Leave this last to prevent overwrites
+        recomp = True
         run_predictors = predictor_list
         recompile_list = predictor_list
-    elif i == "--predictors":
-        for j in sys.argv[count:]:
-            if j == "all":
-                run_predictors = predictor_list
-                break
-            temp = len(run_predictors)
-            for k in predictor_list:
-                if (j == k):
-                    run_predictors.append(k)
-                    break
-            if temp == len(run_predictors):
-                break
-    elif i == "--recompile":
-        for j in sys.argv[count:]:
-            if j == "all":
-                recompile_list = predictor_list
-            temp = len(recompile_list)
-            for k in predictor_list:
-                if (j == k):
-                    recompile_list.append(k)
-                    break
-            if temp == len(recompile_list):
-                break
-        recompile = True
-    elif i == "--help":
-        print ("Avaliable instructions \n" +
-               "--warmup-instructions : Number of warmup instructions to run \n" +
-               "--simulation_instructions : Number of total instructions to run \n" +
-               "--all: compiles and runs all avaliable predictors\n" + 
-               "--predictors: list the predictors you want to run in the format --predictors predictor1 predictor2 ... \n" +
-               "--predictors all: Runs all avaliable predictors\n" +
-               "--recompile: Recompiles the instances of champsim, enable this if you have changed any of the predictor files. this in the format --recompile predictor1 predictor2 \n" +
-               "--recompile all: Recompiles all avaliable predictors, use this command the first time you run the program")
+    
+    return recomp
 
-if recompile:
-    print("Recompiling the following predictors:")
-    print(recompile_list)
-    compile_all(recompile_list)
-
-print(run_predictors)
-
-# find all of the files in the tracer folder, then copy the names into tracelist 
-file_list = os.listdir(tracer)
-tracelist = []
-for i in file_list:
-    if (i.find('.xz') != -1): # filter out only tracer files 
-        tracelist.append(i)
-
+# Filter all tracer files in the tracer folder, then copy the names into tracelist 
+tracelist = [trace for trace in os.listdir(TRACER_PATH) if not trace.find('.xz') != -1]
 
 # from https://stackoverflow.com/questions/30686295/how-do-i-run-multiple-subprocesses-in-parallel-and-wait-for-them-to-finish-in-py
-
 
 # Create a thread for each list of tests, the argument is a list of instructions 
 def create_test(instruction_list, predictor):
@@ -151,3 +201,7 @@ for i in run_predictors:
 
 if (len(run_predictors) > 0):
     display_graph(run_predictors)
+
+
+if __name__ == "__main__":
+    main()
